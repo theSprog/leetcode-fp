60429035b43f69b5512ea8c6f4a5c9251e9ede9d
theSprog <fan.jiaming@hotmail.com>
2025-05-26 22:46:56 +0800

    feat(algorithms): implement twoSum function in C++ and Erlang
    - Add C++ implementation of twoSum using unordered_map
    - Add Erlang implementation of two_sum function using lists and pattern matching

---
+++ src/cpp/algorithms_1.cpp
@@ -0,0 +1,22 @@
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        using Value = int;
+        using Index = int;
+        std::unordered_map<Value, Index> mp;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(mp.count(target - nums[i])) {
+                return {i, mp[target - nums[i]]};
+            }
+            mp.insert({nums[i], i});
+        }
+        
+        return {};
+    }
+};
